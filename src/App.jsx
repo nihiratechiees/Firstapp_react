d469ddb3a49f69415fa554e446846af098a20064
nihiratechiees <nihiratechiees@gmail.com>
2026-06-09 14:27:27 +0530

    useEffect hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import {  useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
@@ -10,20 +10,35 @@ import imgBat from './assets/bat.jpg'
 
 
 function App() {
-  const [count, setCount] = useState(0)
-  const title = "Welcome to React 19.2"
+  let title = "Welcome to React 19.2"
+
+  const [title1,setTitle]=useState("Welcome to React 19.2");
+  const [count,setCount]=useState(0);
 
   const addproduct=({name})=>{
     alert('Product added to cart :' + name)
+    setCount(count+1);
   }
+
+  const updatetitle=(value)=>{
+    title=value;
+    console.log(title);
+  }
+
+  useEffect(()=>{
+    console.log("use effect called");
+  },[count,title1]);
+
 
   return (
     <>
       <MenuHeader></MenuHeader>
-      <h2>{title}</h2>
-      <Products name="MRF Cricket Bat" description="Standard bat" price="10" prodimage={imgBat} onAdd={addproduct}></Products>
-      <Products name="Cricket Ball" price="5" onAdd={addproduct}></Products>
-      <Products name="Cricket Pad" price={20} onAdd={addproduct}></Products>
+      <h2>{title1}</h2>
+      <h2>{count} - products added to cart</h2>
+      <input style={{width:'200px',height:'50px'}} onChange={(e)=> setTitle(e.target.value)}></input>
+      <Products name="MRF Cricket Bat" description="Standard bat" price="10" prodimage={imgBat} onAdd={addproduct} show={true}></Products>
+      <Products name="Cricket Ball" price="5" onAdd={addproduct} show={true}></Products>
+      <Products name="Cricket Pad" price={20} onAdd={addproduct} show={true}></Products>
       <AppFooter></AppFooter>
     </>
 
--- src/Component/Product.jsx
+++ src/Component/Product.jsx
@@ -1,13 +1,25 @@
-const Products = ({ name, description, price,prodimage,onAdd }) => {
-    return (
-        <div className="card">
-            <h2>{name}</h2>
-            <p>{description}</p>
-            <img src={prodimage} className="image"></img>
-            <span>$ {price}</span>
-            <button className="btn" onClick={()=>onAdd({name})}>Buy Now</button>
-        </div>
-    );
+const Products = ({ name, description, price, prodimage, onAdd, show }) => {
+    if (show) {
+        return (
+            <div className="card">
+                <h2>{name}</h2>
+                <p>{description}</p>
+                <img src={prodimage} className="image"></img>
+                <span>$ {price}</span>
+                <button className="btn" onClick={() => onAdd({ name })}>Buy Now</button>
+            </div>
+        );
+    }else{
+        return (
+            <div className="card">
+                <h2>{name}</h2>
+                <p>{description}</p>
+                <img src={prodimage} className="image"></img>
+                <span>$ {price}</span>
+                <button className="btn" onClick={() => onAdd({ name: "no stock" })}>No Stock</button>
+            </div>
+        );
+    }
 }
 
 export default Products;
