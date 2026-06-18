33afa9074b66871c7bb538f67f083fe67075ffa2
nihiratechiees <nihiratechiees@gmail.com>
2026-06-18 18:09:40 +0530

    UseRef hook

--- src/Component/Common/Contact.jsx
+++ src/Component/Common/Contact.jsx
@@ -1,9 +1,36 @@
+import { useEffect, useRef, useState } from "react";
+
 const Contact = () => {
-    return ( 
-        <div>
+    const [count, setCount] = useState(0);
+    let ref=useRef(0);
+    let inputref=useRef(null);
+    useEffect(()=>{
+       // console.log(ref);
+       inputref.current.focus();
+    })
+    const updateref=()=>{
+        ref.current++;
+    }
+    const Getdata=()=>{
+        console.log(inputref.current.value);
+    }
+    return (
+        <div className="card">
             <h1>Contact Us</h1>
+            <h2>Counter value is : {count}</h2>
+            {/* <button style={{"width":"100px"}} onClick={() => setCount(count + 1)} className="btn btn-primary">+</button>
+            <button style={{"width":"100px"}} onClick={() => setCount(count - 1)} className="btn btn-primary">-</button>
+            <button style={{"width":"100px"}} onClick={() => setCount(0)} className="btn btn-primary">Reset</button> */}
+
+            <button style={{"width":"100px"}} onClick={updateref} className="btn btn-primary">Ref +</button>
+             <button style={{"width":"100px"}} onClick={()=>setCount(ref.current)} className="btn btn-primary">Update</button>
+
+
+             <br></br>
+             <input ref={inputref} style={{"width":"200px"}} className="form-control"></input>
+             <button style={{"width":"100px"}} onClick={Getdata} className="btn btn-primary">Getdata</button>
         </div>
-     );
+    );
 }
- 
+
 export default Contact;
