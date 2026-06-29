c5d2324c7897fe64e528acaf0f2f1b264672aee7
nihiratechiees <nihiratechiees@gmail.com>
2026-06-29 19:44:04 +0530

    files added

---
+++ src/Component/Component2.jsx
@@ -0,0 +1,9 @@
+const Component2 = () => {
+    return ( 
+        <div>
+            <h1>Component2</h1>
+        </div>
+     );
+}
+ 
+export default Component2;
---
+++ src/Component/Component3.jsx
@@ -0,0 +1,9 @@
+const Component3 = () => {
+    return ( 
+        <div>
+            <h1>Component 3</h1>
+        </div>
+     );
+}
+ 
+export default Component3;
